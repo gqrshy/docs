535d8a928d7bfe0af662175b1d78dbad23986d0c
gqrshy <garshy.gaming@gmail.com>
2026-01-02 19:41:18 +0900

    rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gashistudios.site',
-	adapter: vercel(),
 	markdown: {
 		shikiConfig: {
 			theme: 'one-dark-pro',
